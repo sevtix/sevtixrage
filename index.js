fe5deb1c824fc8430e44ce6ddc54412366cb8d03
Severin Schmid <sevtixdev@gmail.com>
2019-07-28 09:23:24 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-require('./commands.js')
-require('./events.js')
-require('./vehiclesaving.js')
-
-let spawnBlip = mp.blips.new(120, new mp.Vector3(-2018, -1040, 2),
-    {
-        name: 'Spawnpunkt',
-        shortRange: false,
-});
